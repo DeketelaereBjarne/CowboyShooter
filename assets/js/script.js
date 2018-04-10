a3d90574caa28e3e29e109c3eaeb204e4c1fd156
DeketelaereBjarne <bjarne.deketelaere@student.howest.be>
2018-04-10 11:51:22 +0200

    Added PvP background

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,30 @@
-let config = {
-    type: Phaser.AUTO,
-    width:800,
-    height:600,
-    scene: {
-        preload:preload,
-        create:create,
-        update:update
-    }
-};
 
-let game = new Phaser.Game(config);
+$(document).ready(function(){
+    let config = {
+        type: Phaser.AUTO,
+        width:800,
+        height:600,
+        scene: {
+            preload:preload,
+            create:create,
+            update:update
+        }
+    };
+    let game = new Phaser.Game(config);
+
+});
+
+
 
 function preload(){
-    console.log("preload");
+    console.log("preload called");
+    this.load.image('sky','assets/media/pvpbackground.jpg');
 }
 function create(){
-    console.log("create");
-    
+    console.log("create called");
+    let background = this.add.sprite(400,300,'sky');
+    background.scaleX = 1.2;
+    background.scaleY = 1.25;
 }
 function update() {
     
